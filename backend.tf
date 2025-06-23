88468223453c89e3a40404a647d1be0cf8ebc636
Glauber GF <glauber_gf@hotmail.com>
2025-06-23 17:00:17 -0300

    chore: removido backend e workflows

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "homelabmcn" # https://app.terraform.io/app/organizations
-
-    workspaces {
-      name = "terraform-proxmox-debian-syspass-dev"
-    }
-  }
-}
